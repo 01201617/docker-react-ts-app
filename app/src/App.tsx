c35db6bd271113a3edc07657c82839fe25896047
nan <torako.04saibino4ban@gmail.com>
2022-08-27 18:39:09 +0900

    create count with useState

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  // console.log("見える？")
+  const [count, setCount] = useState(0)
+  const countUp = () =>{setCount(count+1)}
+  const countDown = () =>{setCount(preCount=>preCount-1)}
+  const reset = () => setCount(0)
+  const dividedBy3 = () => setCount(preCount =>{
+    return preCount%3 ===0 ? preCount/3 : preCount
+  })
   return (
-    <div>
-      This is a template for React app
-    </div>
+    <>
+      <p>count: {count}</p>
+      <button onClick={countUp}>+1</button>
+      <button onClick={countDown}>-1</button>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={dividedBy3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
   );
 }
 
